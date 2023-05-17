73c0de0fb23a6516e222b98c2e9db06951284670
Anthy Chen <koyun.chen@gmail.com>
2023-05-17 13:49:02 -0700

    Initial commit

---
+++ Multable/MultableApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultableApp.swift
+//  Multable
+//
+//  Created by Anthy Chen on 5/17/23.
+//
+
+import SwiftUI
+
+@main
+struct MultableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
